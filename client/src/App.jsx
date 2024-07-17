248027659d2862eb918f15bfad5f0fbf46339cde
dikshagupta3094 <dikshagpragya@gmail.com>
2024-07-17 19:47:52 +0530

    added most of the dependencies of the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-     <h1 className="text-orange-500">Hello</h1>
+     <h1 className="text-orange-500 text-center text-3xl">Hello</h1>
     </>
   );
 }
