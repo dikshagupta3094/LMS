2c6a75c6c73e5fbdf65f23931d11c3fc7d2593df
dikshagupta3094 <dikshagpragya@gmail.com>
2024-07-21 19:38:07 +0530

    add hot toaster

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import { Route,Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path=""></Route>
+    </Routes>
      <h1 className="text-orange-500 text-center text-3xl">Hello</h1>
     </>
   );
