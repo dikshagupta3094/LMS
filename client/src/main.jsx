14b1dd06fcf3e47e67aef1a227d5be31945110a0
dikshagupta3094 <dikshagpragya@gmail.com>
2024-07-22 05:15:04 +0530

    redux store setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import './index.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Toaster} from 'react-hot-toast'
 import {BrowserRouter} from 'react-router-dom'
-
+import {Provider} from 'react-redux'
 import App from './App.jsx'
+import store from './Redux/store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+   <Provider store={store}>
    <BrowserRouter>
       <App />
       <Toaster/>
     </BrowserRouter>
- 
+    </Provider>
 )
