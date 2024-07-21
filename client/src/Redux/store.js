b442e44b90eb72521f615584df6bfd441e5a1a84
dikshagupta3094 <dikshagpragya@gmail.com>
2024-07-22 05:13:18 +0530

    redux basic auth setup

---
+++ client/src/Redux/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit'
+import {authSliceReducer} from './Slices/AuthSlice'
+
+ const store = configureStore({
+    reducer:{authSliceReducer},
+    devTools:true
+ })
+
+ export default store
