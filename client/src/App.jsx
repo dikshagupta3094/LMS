361eed05ee6476461dcd8a16d78270eb3d9bf27f
dikshagupta3094 <dikshagpragya@gmail.com>
2024-08-20 12:50:38 +0530

    Added Denied Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import CourseList from "./Pages/CourseList";
 import Contact from "./Pages/Contact";
+import Denied from "./Pages/Denied";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/courses" element={<CourseList/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/denied" element={<Denied/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </>
---
+++ client/src/Pages/Denied.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Denied() {
+    const navigate = useNavigate();
+  return (
+    <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold tracking-widest text-white">
+        403
+      </h1>
+      <div className="text-sm bg-black text-white rounded rotate-12 px-2 absolute">
+        Access Denied
+      </div>
+      <button onClick={()=>navigate(-1)} className="mt-5"><span className="relative block px-8 py-3 bg-[#1A2238] border border-current font-semibold">Go Back</span></button>
+    </main>
+  );
+}
+
+export default Denied;
