9712dde25f054f705b3ae8719e449df54ee5f10a
dikshagupta3094 <dikshagpragya@gmail.com>
2024-09-08 22:03:06 +0530

    Added CourseDes and Create Course Page

---
+++ client/src/Pages/CreateCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateCourse() {
+  return (
+    <div>
+      <h1 className='text-center text-4xl text-white'>Course</h1>
+    </div>
+  )
+}
+
+export default CreateCourse
